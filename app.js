bf8480851f8019df4e41d51ffc9160418b86eee5
Annely Kurg <annely.kurg@khk.ee>
2021-12-20 20:27:29 +0200

    Displayed city weather data from API

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ function weatherDataFetch(city) {
     var key = 'feabf737c0396f2a57dddb353e18684c';
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
         .then(function(resp) {
-            returnresp.json()
+            return resp.json()
         }) //Convert data to json
         .then(function(data) {
             console.log(data);
+            drawWeather(data);
         })
         .catch(function(){
             //catch any errors
@@ -16,4 +17,13 @@ function weatherDataFetch(city) {
 
 function cityWeather(e) {
     weatherDataFetch( 'Tallinn');
+}
+
+function drawWeather( data ) {
+    var celsius = Math.round(parseFloat(data.main.temp)-273.15);
+    var description = data.weather[0].description;
+
+    document.querySelector('#description').innerHTML = description;
+    document.querySelector('#temp').innerHTML = celsius + '&deg';
+    document.querySelector('#location').innerHTML = data.name;
 }
